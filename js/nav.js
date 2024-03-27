0d2f06e31b30735798a6b268f15f3ce93e5273c5
agus <agusmian18@hotmail.com>
2024-03-27 16:33:39 -0300

    Actualizacion Header v0.3

--- js/nav.js
+++ js/nav.js
@@ -1,9 +1,13 @@
-function openNav() {
-        document.getElementById("mobile-menu").style.width = "100%";
-    }
 
-    function closeNav() {
-        document.getElementById("mobile-menu").style.width = "0%";
+const navToggle = document.querySelector(".nav-toggle");
+const navMenuLinks = document.querySelector(".nav-links");
+
+navToggle.addEventListener("click", () => {
+    navMenuLinks.classList.toggle("nav-links_visible");
+
+    if (navMenuLinks.classList.contains("nav-links_visible")) {
+        navToggle.setAttribute("aria-label", "Cerrar menu");
+    } else {
+        navToggle.setAttribute("aria-label", "Abrir menu");
     }
-   
-   
+}) 
